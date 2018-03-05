b502fb1af8431c6ec1fbfac0b98943dcd7daab74
Lithinn <34646168+Lithinn@users.noreply.github.com>
2018-03-05 21:31:23 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  char c;  
+  scanf("%c", &c);
+  if ((c>= 'a' && c<= 'z') || (c >= 'A' &&c<= 'Z')) {
+    if (c=='a' || c=='A' || c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' || c== 'u' || c=='U')
+      printf("%c is a vowel.\n", c);
+    else
+      printf("%c is a constant.\n", c);
+  }
+  else
+    printf("%c is neither a vowel nor a consonant.\n", c);
+    return 0;
+ 
