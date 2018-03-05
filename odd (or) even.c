1de300c1f890bb23b29e66f8f80a561279f53263
Lithinn <34646168+Lithinn@users.noreply.github.com>
2018-03-05 21:21:43 +0530

    Create odd (or) even.c

---
+++ odd (or) even.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main(void) {
+	int a=5;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+    	printf("even");
+    }
+    else
+    {
+    	printf("odd");
+    }
+	return 0;
+}
