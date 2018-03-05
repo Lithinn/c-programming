bc6899c274b14dc2a83fb484a615e64c555c30b7
Lithinn <34646168+Lithinn@users.noreply.github.com>
2018-03-05 21:06:47 +0530

    Create set1.c

---
+++ set1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a=10;
+	printf("enter no:");
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("positive case");
+		
+	}
+	else {
+		printf("negative");
+	}
+	return 0;
+}
