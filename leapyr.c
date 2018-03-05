045f14026a9fd0f87f5de663ccad03c463dbeb5e
Lithinn <34646168+Lithinn@users.noreply.github.com>
2018-03-05 22:33:09 +0530

    Update leapyr.c

--- leapyr.c
+++ leapyr.c
@@ -2,14 +2,14 @@ void main()
 {
     int year;
  
-    printf("Enter a year \n");
+    printf("Enter year \n");
     scanf("%d", &year);
     if ((year % 400) == 0)
-        printf("%d is a leap year \n", year);
+        printf("%d is leap year \n", year);
     else if ((year % 100) == 0)
-        printf("%d is a not leap year \n", year);
+        printf("%d is not leap year \n", year);
     else if ((year % 4) == 0)
-        printf("%d is a leap year \n", year);
+        printf("%d is leap year \n", year);
     else
-        printf("%d is not a leap year \n", year);
+        printf("%d is not leap year \n", year);
 }
