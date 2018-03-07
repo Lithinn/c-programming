9f51e564b17064bd1ded26ec0c0ef90375807d49
Lithinn <34646168+Lithinn@users.noreply.github.com>
2018-03-07 23:26:02 +0530

    Update multiples.cpp

--- multiples.cpp
+++ multiples.cpp
@@ -4,12 +4,11 @@ void main()
 {
         longint sum=0,i,n;
         clrscr();
-        printf("\n Please Give The Value of N:  ");
+        printf("\n Value of N:  ");
         scanf("%ld",&n);
         for(i=1;i<=n;i++)
         {
-            printf("\n 5 * %ld = %ld",i,5*i);
+        printf("\n 5 * %ld = %ld",i,5*i);
         }
-        
         getch();
 }
