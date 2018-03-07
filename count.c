ba647fb62fa7bc473e3d3db197ebd9b8afd18c97
Lithinn <34646168+Lithinn@users.noreply.github.com>
2018-03-07 20:26:52 +0530

    Update count.c

--- count.c
+++ count.c
@@ -4,10 +4,11 @@ int main()
 long n;
 int count = 0;
 printf("Enter integer:");
-scanf("%lld", &n); 
+scanf("%d", &n); 
 while(n!= 0) 
-{ // n = n/10 n /= 10;
+{
+  n /= 10;
 ++count;
 }
-printf("Number of digits: %d", count); 
+printf("Number of digit: %d",count); 
 }
