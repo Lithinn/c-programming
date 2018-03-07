20a530aa47bcd3e7a4b5421eae19ad2bd5b44798
Lithinn <34646168+Lithinn@users.noreply.github.com>
2018-03-07 21:54:25 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+   int n, i = 3, count, c;
+   printf("Enter the prime numbers\n");
+   scanf("%d",&n);
+   if ( n >= 1 )
+   {
+    printf("%d prime numbers are :\n",n);
+    printf("2\n");
+   }
+   for ( count = 2 ; count <= n ;  )
+   {
+    for ( c = 2 ; c <= i - 1 ; c++ )
+    {
+    if ( i%c == 0 )
+    break;
+    }
+    if ( c == i )
+    {
+    printf("%d\n", i);
+    count++;
+    }
+   i++;}
+   return 0;
+}
